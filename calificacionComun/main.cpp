3e1e8594859e6df62a88eeed51792fbe85057699
Mario <mariogarzac99@gmail.com>
2020-02-21 15:50:52 -0600

    Initial Commit

---
+++ calificacionComun/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  calificacionComun
+//
+//  Created by Mario Garza Chapa on 21/02/20.
+//  Copyright © 2020 Mario Garza Chapa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
